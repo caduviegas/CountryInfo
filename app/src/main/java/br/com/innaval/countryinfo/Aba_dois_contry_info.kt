bf725b8900ee3966c1612ece6c482ed486173963
Eduardo Viegas <engenedu@hotmail.com>
2020-11-28 19:10:29 -0300

    Introdução dos dois botÃões de cada layout

---
+++ app/src/main/java/br/com/innaval/countryinfo/Aba_dois_contry_info.kt
@@ -0,0 +1,11 @@
+package br.com.innaval.countryinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Aba_dois_contry_info : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aba_dois_contry_info)
+    }
+}
--- app/src/main/java/br/com/innaval/countryinfo/MainActivity.kt
+++ app/src/main/java/br/com/innaval/countryinfo/MainActivity.kt
@@ -1,11 +1,20 @@
 package br.com.innaval.countryinfo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonUm = findViewById<Button>(R.id.button_um)
+
+        buttonUm.setOnClickListener {
+            val abrirButton = Intent(this,Aba_dois_contry_info:: class.java )
+            startActivity(abrirButton)
+        }
     }
 }
